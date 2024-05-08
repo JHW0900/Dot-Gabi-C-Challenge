fffe83996feb7c5032971fd44edd6b070539754a
ChangSeopp <168637925+ChangSeopp@users.noreply.github.com>
2024-05-08 13:51:50 +0900

    1w-02.c

--- 이창섭/1w-02.c
+++ 이창섭/1w-02.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#define _CRT_NO_SECURE_WARNINGS
 
+int main(void)
+{
+	int star, i, j, k;
+	scanf_s("%d", &star); //star=5대입
+	for (i = 1; i <= star; i++) //1<=5 ->{}실행후 i=2
+	{
+		for (j = 1; j <= star - i; j++)
+		{
+			printf(" ");
+		}
+		for (k = 1; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
